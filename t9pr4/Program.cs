ab1b7390a9d11f03aa4c2f8bd5c779cbb0333ddf
DEX2207 <123413987+DEX2207@users.noreply.github.com>
2023-02-27 10:23:38 +0200

    Update Program.cs

--- t9pr4/Program.cs
+++ t9pr4/Program.cs
@@ -9,7 +9,24 @@ namespace t9pr4
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {//1
+            int[] mass = new int[15];
+            Random rnd =new Random();
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rnd.Next(-50,50); 
+                Console.WriteLine($"mass[{i}]={mass[i]}");
+            }
+            int a = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i]%3==0)
+                {
+                    a++;
+                }
+            }
+            Console.WriteLine($"Кол-во чисел, кратное трем, равно {a}");
+            Console.ReadLine();
         }
     }
 }
